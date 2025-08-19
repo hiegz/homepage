f35a33ce2151d6335e5e60a4229a7571964c915a
Volodymyr But <git@hiegz.com>
2025-08-19 20:49:48 +0200

    move the LinkedIn icon next to the CV download button

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
 let body = document.getElementById("body");
 
-let links   = document.querySelectorAll("#links > *"); // prettier-ignore
-let profile = document.getElementById("profile"); // prettier-ignore
-let cv      = document.getElementById("cv-container"); // prettier-ignore
+let header = document.querySelector("header"); // prettier-ignore
+let main   = document.querySelector("main"); // prettier-ignore
+let footer = document.querySelector("footer"); // prettier-ignore
 
 // ---
 
 const INTRO_DURATION = 1000;
 
-profile.style.opacity = 0;
-cv.style.opacity = 0;
-links.forEach((child, _) => (child.style.opacity = 0));
+for (let i = 0; i < header.children.length; ++i)
+    header.children[i].style.opacity = 0;
+main.style.opacity = 0;
+for (let i = 0; i < footer.children.length; ++i)
+    footer.children[i].style.opacity = 0;
 
 window.onload = async function () {
     body.classList.add("loaded");
 
     (async () => {
-        await transition(
-            profile,
-            { opacity: 0 },
-            { opacity: 1 },
-            INTRO_DURATION,
-        );
-        float(profile);
+        await transition(main, { opacity: 0 }, { opacity: 1 }, INTRO_DURATION);
+        float(main);
     })();
 
     await delay(300);
 
-    (async () => {
-        await transition(
-            cv,
-            { opacity: 0, transform: "translateY(5px)" },
-            { opacity: 1, transform: "translateY(0)" },
-            INTRO_DURATION,
-        );
-        float(cv);
-    })();
-
-    await delay(300);
-
-    for (let i = 0; i < links.length; ++i) {
+    for (let i = 0; i < footer.children.length; ++i) {
         (async () => {
             await transition(
-                links[i],
+                footer.children[i],
+                { opacity: 0, transform: "translateY(5px)" },
+                { opacity: 1, transform: "translateY(0)" },
+                INTRO_DURATION,
+            );
+            float(footer.children[i]);
+        })();
+
+        await delay(300);
+    }
+
+    for (let i = 0; i < header.children.length; ++i) {
+        (async () => {
+            await transition(
+                header.children[i],
                 { opacity: 0, transform: "translateY(-5px)" },
                 { opacity: 1, transform: "translateY(0)" },
                 INTRO_DURATION,
             );
-            float(links[i]);
+            float(header.children[i]);
         })();
 
         await delay(300);
