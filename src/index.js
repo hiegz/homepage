216de9264b67bbdd8ed5a47ebe637ddc45e1b025
Volodymyr But <git@hiegz.com>
2025-09-01 21:19:54 +0200

    add password prompt with simulated entry

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import $ from "jquery";
 import clock from "./clock.js";
 
+import login from "./login/view.js";
+
 import "./style.css";
 
 // import index.html so that webpack can watch and refresh the file whenever it is changed
@@ -8,7 +10,9 @@ if (process.env.NODE_ENV === "development") {
     import("./index.html");
 }
 
-window.addEventListener("load", function () {
+window.addEventListener("load", async function () {
     $("#wallpaper").css("display", "initial");
     clock.start();
+
+    await login.load();
 });
---
+++ src/login/view.js
@@ -0,0 +1,27 @@
+import $ from "jquery";
+import html from "./view.html";
+
+/**
+ * --
+ */
+async function load() {
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+    const randomInBetween = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    $("body").append(html);
+
+    await delay(500);
+
+    // simulate typing
+    for (let i = 0; i < 10; ++i) {
+        const currentVal = $("#password-input").val();
+        $("#password-input").val(currentVal + "-");
+        await delay(randomInBetween(70, 150));
+    }
+}
+
+/* */
+
+export default { load };
