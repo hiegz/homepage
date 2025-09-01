396cb64aaeb5dd3ff89ffbcea9060734863ef5d8
Volodymyr But <git@hiegz.com>
2025-09-01 10:45:10 +0200

    add webpack html loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("src/index.js loaded")
+// import index.html so that webpack can watch and refresh the file whenever it is changed
+if (process.env.NODE_ENV === "development") {
+    import("./index.html");
+}
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
     entry: "./src/index.js",
@@ -6,4 +7,13 @@ export default {
         filename: "main.js",
         path: path.resolve(import.meta.dirname, "dist"),
     },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
 };
