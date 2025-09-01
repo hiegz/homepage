71487f052b707e15db7c5ad18e7ba937bf56b813
Volodymyr But <git@hiegz.com>
2025-09-01 17:42:08 +0200

    add desktop wallpaper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import $ from "jquery";
+
 import "./style.css";
 
 // import index.html so that webpack can watch and refresh the file whenever it is changed
 if (process.env.NODE_ENV === "development") {
     import("./index.html");
 }
+
+window.addEventListener("load", function () {
+    $("#wallpaper").css("display", "initial");
+});
