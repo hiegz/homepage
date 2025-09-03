7a90f5623aacd0450519f7b4b2976797ef93575d
Volodymyr But <git@hiegz.com>
2025-09-03 19:15:33 +0200

    prep for upcoming rewrite

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
-
-import $ from "jquery";
-import clock from "./clock.js";
-
-import login from "./login.js";
-import iconGrid from "./icon-grid.js";
-import folder from "./folder.js";
-
 import "./style.css";
 
 // import index.html so that webpack can watch and refresh the file whenever it is changed
@@ -14,11 +6,4 @@ if (process.env.NODE_ENV === "development") {
     import("./index.html");
 }
 
-window.addEventListener("load", async function () {
-    $("#wallpaper").css("display", "initial");
-    clock.start();
-
-    await login();
-
-    iconGrid.$view.appendTo("#desktop");
-});
+window.addEventListener("load", async function () {});
