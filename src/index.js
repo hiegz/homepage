aa607a2913fa5cd8b1d7a8740350927fa4ed3309
Volodymyr But <git@hiegz.com>
2025-09-04 20:42:45 +0200

    restore the wallpaper

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import "./style.css";
 
+import $ from "jquery";
+
 // import index.html so that webpack can watch and refresh the file whenever it is changed
 if (process.env.NODE_ENV === "development") {
     import("./index.html");
 }
 
-window.addEventListener("load", async function () {});
+window.addEventListener("load", async function () {
+    // display the wallpaper
+    $(".wallpaper").css("display", "");
+});
